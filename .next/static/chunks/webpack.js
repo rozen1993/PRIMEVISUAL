b77a5218f9905eed44bdc29a9fdd06724ef8aecb
Rozen <tigole_93@hotmail.com>
2024-09-14 09:12:47 -0500

    Safari2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "badcba47ff294558"; }
+/******/ 		__webpack_require__.h = function() { return "c954d0781a9ef1aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
