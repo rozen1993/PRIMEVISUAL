a06d1c054181fd8ddfc36e45deda2bbf288528ac
creator <m03449096@gmail.com>
2025-12-06 22:24:15 -0500

    GoogleTag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,22 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           {/* meta end */}
 
+          {/* Google tag (gtag.js) */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-17615529885"
+          ></script>
+
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17615529885');
+            `,
+          }} />
+
+
           {/* public assets begin */}
           <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" />
           <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" />
